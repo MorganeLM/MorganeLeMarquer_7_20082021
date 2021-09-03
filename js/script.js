044691d90606ac9af5b226b7d91cf4ce2a08df0f
MorganeLM <morganelemarquer@lilo.org>
2021-09-03 14:59:48 +0200

    search by filter methode

--- js/script.js
+++ js/script.js
@@ -8,4 +8,28 @@ let resultListElement = document.querySelector('#result-list');
 resultListElement.innerHTML = RECIPES[0]['name'];
 
 let recipes = RECIPES;
+let searchValue = 'chocolat'
 
+let recipesSorted = recipes.filter((recipe) => {
+  return recipe.name.toLowerCase().includes(searchValue) &&  
+  recipe.ingredients.some((i) => i.ingredient.toLowerCase().includes(searchValue)) &&  
+  recipe.description.toLowerCase().includes(searchValue);
+});
+
+console.log(recipesSorted)
+
+console.log(tagModule.getIngredientForTags(recipesSorted));
+console.log(tagModule.getUstensilForTags(recipesSorted));
+console.log(tagModule.getApplianceForTags(recipesSorted));
+
+
+
+// function research(request, appliance, ustensil, ingredients) {
+//   let result = data.recipes.filter(recipe => 
+//       matchAppliance(recipe, appliance) 
+//       && matchUstensils(recipe, ustensil) 
+//       && matchTagsIngredients(recipe, ingredients)
+//       && ( matchName(recipe, request) || matchDescriptions(recipe, request) || matchIngredients(recipe, request))
+//   );
+//   return result;
+// }
